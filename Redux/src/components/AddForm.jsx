7305bc0d9e743435efb534b36a3925cfc828c7cb
Its-Adarshkumar <adarshsinghrajput414@gmail.com>
2025-08-08 23:53:03 +0530

    REDUX TOOLKIT

---
+++ Redux/src/components/AddForm.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
+
+export default function AddForm(){
+    const [task,setTask]=useState("");
+    const dispatch= useDispatch();
+
+    const submitHandler=(e)=>{
+        e.preventDefault();
+        console.log(task);
+        dispatch(addTodo(task));  //action.payload=task    
+        setTask(" ");
+    };
+
+    return (
+        <>
+          <form onSubmit={submitHandler} >
+            <input type="text" onChange={(e)=>setTask(e.target.value)}/>
+            <button>Add Task</button>
+
+          </form>
+        </>
+    )
+}
